995e54ff9d2a7e19c522e7802e0b64094cf50798
dinisoraya <dinisoraya723@gmail.com>
2024-10-20 03:09:56 +0700

    feat: add upload file

--- database/migrations/2024_10_07_135405_add_foto_to_users_table.php.php
+++ database/migrations/2024_10_07_135405_add_foto_to_users_table.php.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('user', function (Blueprint $table) { 
-            $table->string('foto')->nullable();
+        Schema::table('users', function (Blueprint $table) {
+            //
         });
     }
 
@@ -21,8 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('user', function (Blueprint $table) { 
-            $table->dropColumn('foto');
-        }); 
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
